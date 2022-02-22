bce7281418ecde4a24372cbee26ce6ae6795ff72
Andrei Semeniuk <semeniukandrei@gmail.com>
2022-02-22 20:26:23 +0300

    before change folders

---
+++ src/app/components/product-list/product-list.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { ProductService } from "../../services/product.service";
+import { ProductModel } from "../../services/product-model";
+import { Category } from '../first/first.component';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.component.html',
+  styleUrls: ['./product-list.component.css']
+})
+export class ProductListComponent implements OnInit {
+
+  products: ProductModel[] = [new ProductModel ("","", 0,Category.Undefined,
+  false,['', ''], ['', ''])];
+
+  constructor(private productService: ProductService) { }
+  
+
+  ngOnInit(): void {
+    this.getProducts();
+  }
+
+  getProducts(){
+   this.products=this.productService.getProducts();
+  }
+
+}
